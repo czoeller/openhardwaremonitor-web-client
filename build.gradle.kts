bd338a5771c18c505e96ba9666fe433983c6d7ca
czoeller <21084292+czoeller@users.noreply.github.com>
2026-03-12 18:46:50 +0100

    downgrade and see

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.gradle.jvm.tasks.Jar
 
 plugins {
-    kotlin("jvm") version "2.3.0"
-    kotlin("plugin.serialization") version "2.3.0"
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.serialization") version "2.2.0"
     id("org.jetbrains.dokka") version "2.1.0"
     id("org.jetbrains.dokka-javadoc") version "2.1.0"
     `java-library`
